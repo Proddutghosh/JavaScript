8bf9e9a01da059732d2c3a6db40f11f00d757955
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-13 16:51:49 +0600

    updates

--- variablesAndDataTypes.js
+++ variablesAndDataTypes.js
@@ -15,8 +15,10 @@ console.log(job);
 job = "Teacher";
 console.log(job);
 
+/* 
 // Variable naming rules
 
 var _3years = 3;
 var johnMark = 'John and MArk';
 //var if = 23; // it doesn't exits because if is reserved variable
+ */

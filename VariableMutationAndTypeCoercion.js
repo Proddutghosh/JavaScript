7e33a78d6c7d7d88069647801a0856ca8abc9944
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-15 00:51:57 +0600

    updates

--- VariableMutationAndTypeCoercion.js
+++ VariableMutationAndTypeCoercion.js
@@ -3,3 +3,9 @@ var age = 28;
 
 // Type coercion
 console.log(firstName + ' ' + age);
+
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);

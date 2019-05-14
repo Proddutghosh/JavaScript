d6a3781daad5e2a5d89fdf52f5739e4eb08f5fa7
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-15 00:53:41 +0600

    updates

--- VariableMutationAndTypeCoercion.js
+++ VariableMutationAndTypeCoercion.js
@@ -1,19 +1,33 @@
-var firstName = 'John';
+var firstName = "John";
 var age = 28;
 
 // Type coercion
-console.log(firstName + ' ' + age);
-
+console.log(firstName + " " + age);
 
 var job, isMarried;
-job = 'teacher';
+job = "teacher";
 isMarried = false;
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
 
 // Variable mutation
-age = 'twenty eight';
-job = 'driver';
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-var lastName = prompt('What is his last Name?');
-console.log(firstName + ' ' + lastName);
+age = "twenty eight";
+job = "driver";
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+var lastName = prompt("What is his last Name?");
+console.log(firstName + " " + lastName);

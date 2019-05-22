b224e0260103cb512874f237bab4559bc02f3cf2
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-22 21:49:56 +0600

    updates

--- BasicOperators.js
+++ BasicOperators.js
@@ -3,9 +3,12 @@ now = 2018;
 ageJohn = 28;
 ageMark = 33;
 
-
 // Math operators
 yearJohn = now - ageJohn;
 yeahMark = now - ageMark;
 
 console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);

de7acd62cbfd7cfc7edfb9d1db550c8466e15d27
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-23 18:46:21 +0600

    updates

--- OperatorPrecedence.js
+++ OperatorPrecedence.js
@@ -12,7 +12,7 @@ var ageMark = 35;
 var average = (ageJohn + ageMark) / 2;
 console.log(average);
 
-
+// Multiple assignments
 var x, y;
 x = y = (3 + 5) * 4 - 6;  // 8 * 4 - 6 // 32 - 6 // 26
 console.log(x, y);

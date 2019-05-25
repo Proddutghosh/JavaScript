8e17e0a00dcbe42bc4c38115578ee2bbbd19ad78
Lakshman Gope <lakshmangope012@gmail.com>
2019-05-26 02:35:27 +0600

    updates

--- CodingChallenge1.js
+++ CodingChallenge1.js
@@ -14,3 +14,7 @@ var heightMark = 1.69; // meters
 
 var massJohn = 92;
 var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
